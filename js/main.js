397ceff0589b81529e237c1ce413a25ce871709d
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2022-06-29 17:17:42 -0400

    can render the image and name of each pokemon to the browser

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
+const container = document.querySelector('.container');
+const colors = {
+  normal: '#a8a878',
+  fire: '#f08030',
+  fighting: '#c03028',
+  water: '#6890f0',
+  grass: '#78c850',
+  poison: '#a040a0',
+  electric: '#f8d030',
+  ground: '#e0c068',
+  psychic: '#f85888',
+  rock: '#b8a038',
+  ice: '#98d8d8',
+  bug: '#a8b820',
+  dragon: '#7038f8',
+  ghost: '#705898',
+  fairy: '#ee99ac',
+};
+
 const getPokemon = (limit) => {
   fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}`)
     .then((res) => res.json())
@@ -12,7 +31,18 @@ const getPokemon = (limit) => {
 };
 
 const renderPokemon = (pokemon) => {
-  console.log(pokemon);
+  const pokeContainer = document.createElement('div');
+  const image = document.createElement('img');
+  const name = document.createElement('h2');
+
+  pokeContainer.classList.add('pokemon');
+  pokeContainer.style.backgroundColor = colors[pokemon.types[0].type.name];
+
+  image.src = pokemon.sprites.front_default;
+  name.textContent = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+
+  pokeContainer.append(image, name);
+  container.append(pokeContainer);
 };
 
-getPokemon(6);
+getPokemon(151);
