72fb6db3e1059d1e77fe1e26d184062a94826be2
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2022-06-29 15:04:47 -0400

    can fetch pokemon data from pokeapi

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-console.log('hi');
+const getPokemon = (limit) => {
+  fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}`)
+    .then((res) => res.json())
+    .then(({ results }) =>
+      Promise.all(results.map((result) => fetch(result.url)))
+    )
+    .then((responses) => Promise.all(responses.map((res) => res.json())))
+    .then((pokemon) => pokemon.forEach((pokemon) => renderPokemon(pokemon)))
+    .catch((err) => {
+      console.log(`error: ${err}`);
+    });
+};
+
+const renderPokemon = (pokemon) => {
+  console.log(pokemon);
+};
+
+getPokemon(6);
